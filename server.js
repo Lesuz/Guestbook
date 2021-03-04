f9962be8f4180a8b270c534438970596584f4c32
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-03-04 15:18:05 +0200

    added styling and amde website look cleaner

--- server.js
+++ server.js
@@ -11,33 +11,55 @@ app.get("/", function (req, res) {
     res.sendFile( __dirname + "/public/index.html");
 });
 
-app.get("/public/guestbook", function (req, res) {
+app.get("/guestbook", function (req, res) {
 
     let data = require("./data.json");
 
-    console.log(data);
+    let guestbook = '<!DOCTYPE html>' +
+    '<html>' +
+    '<head>' +
+        '<link rel="stylesheet" href="./css/styles.css">'+
+        '<script src="../server.js"></script>'+
+        '<title>Guestbook</title>'+
+    '</head>'+
+    '<body>'+
+        '<nav class="navbar">'+
+            '<p>Guestbook</p>'+
+            '<ul>' +
+                '<li><a href="/">Home</a></li> ' +
 
-    let guestbook = "<table border='1' style='width:100%;'>" +
-                    "<tr style='width:100%'><th style='width:90%' >Message</th><th>Username</th><th>Country</th></tr>";
+                '<li><a href="/guestbook">Guestbook</a></li>'+
 
-    for (let i = 0; i < data.length; i++) {
-        guestbook += 
-        "<tr><td>" + data[i].message + "</td>" +
-        "<td>" + data[i].username + "</td>" + 
-        "<td>" + data[i].country + "</td></tr>";
-    };
-    guestbook += "</table>";
+                '<li><a href="/newmessage">New Message</a></li>'+
+
+                '<li><a href="/ajaxmessage">AJAX messages</a></li>'+
+
+            '</ul>'+
+        '</nav>'+
+        '<table>' +
+            '<tr><th>Message</th><th>Username</th><th>Country</th></tr>';
+            
+            for (let i = 0; i < data.length; i++) {
+                guestbook += 
+                "<tr><td>" + data[i].message + "</td>" +
+                "<td>" + data[i].username + "</td>" + 
+                "<td>" + data[i].country + "</td></tr>";
+            };
+            guestbook += 
+            '</table>' +
+        '</body>'+
+    '</html>' ;
 
     // res.send() automatically takes care of content-type and also ends the response
     res.send(guestbook);
     
 });
 
-app.get("/public/newmessage", function (req, res) {
+app.get("/newmessage", function (req, res) {
     res.sendFile( __dirname + "/public/newmessage.html");
 
 });
-app.post("/public/newmessage", function (req,res) {
+app.post("/newmessage", function (req,res) {
     
     let data = require("./data.json");
 
@@ -54,12 +76,32 @@ app.post("/public/newmessage", function (req,res) {
     fs.writeFile("./data.json", dataStringify, function (err) {
         if (err) return console.log(err);
     });
-    res.redirect("/public/guestbook");
+    res.redirect("/guestbook");
     
 });
 app.get("/ajaxmessage", function (req, res) {
     res.sendFile( __dirname + "/public/ajaxmessage.html");
 });
+app.post("/ajaxmessage", function (req, res) {
+   
+    let data = require("./data.json");
+
+    data.push( {
+        "username": req.body.username,
+        "country": req.body.country,
+        "date" : new Date(),
+        "message": req.body.message
+    });
+
+    // Stringify the updated json array and write it into a file
+    let dataStringify = JSON.stringify(data);
+
+    fs.writeFile("./data.json", dataStringify, function (err) {
+        if (err) return console.log(err);
+    });
+    
+});
+
 app.get("*", function (req, res) {
     res.send("Can't find requested page");
 });
